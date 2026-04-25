702bcac89b95d99d6b2bdb70953b6d5788db7e2e
Katherine Malakhova <katherinemalakhova@gmail.com>
2026-04-25 20:51:23 +0300

    Feat/singin endpoint (#20)
    * Fix errors
    
    * Implement /auth/singin endpoint
    
    * Refactoring

---
+++ src/main/java/com/nanashe/backend/dto/auth/request/SignInRequestDto.java
@@ -0,0 +1,4 @@
+package com.nanashe.backend.dto.auth.request;
+
+public record SignInRequestDto(String email, String password) {
+}
--- src/main/java/com/nanashe/backend/service/UserService.java
+++ src/main/java/com/nanashe/backend/service/UserService.java
@@ -1,11 +1,14 @@
 package com.nanashe.backend.service;
 
-import com.nanashe.backend.dto.auth.request.SignupRequestDto;
+import com.nanashe.backend.dto.auth.request.SignInRequestDto;
+import com.nanashe.backend.dto.auth.request.SignUpRequestDto;
 import com.nanashe.backend.entity.User;
 import com.nanashe.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
@@ -15,8 +18,9 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public void signup(SignupRequestDto dto) {
+    public void signUp(SignUpRequestDto dto) {
         User user = User.builder()
                 .username(dto.username())
                 .email(dto.email())
@@ -24,5 +28,15 @@ public class UserService {
                 .joined(OffsetDateTime.now())
                 .build();
         userRepository.save(user);
+    }
+
+    public String signIn(SignInRequestDto dto) {
+        String token = userRepository.findByEmail(dto.email())
+                .filter(user -> passwordEncoder.matches(dto.password(), user.getPasswordHash()))
+                .map(User::getId)
+                .map(jwtService::generateAccessToken)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials"));
+
+        return "Bearer " + token;
     }
 }
