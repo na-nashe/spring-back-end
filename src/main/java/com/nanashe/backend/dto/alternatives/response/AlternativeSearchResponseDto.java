2cd62f8d486cd3e95fc2184081df45bab051e7af
extziz <katherinemalakhova@gmail.com>
2026-05-15 14:49:56 +0300

    Ignore the extra fields from the Python response

--- src/main/java/com/nanashe/backend/dto/alternatives/response/AlternativeSearchResponseDto.java
+++ src/main/java/com/nanashe/backend/dto/alternatives/response/AlternativeSearchResponseDto.java
@@ -1,7 +1,10 @@
 package com.nanashe.backend.dto.alternatives.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record AlternativeSearchResponseDto(
         String message,
         List<AlternativeResponseDto> alternatives
